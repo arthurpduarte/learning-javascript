289a478f9c3085a0e4cc48b01ecd3fa6736bae5c
Arthur P <tech@apduarte.com>
2018-11-24 11:24:08 -0800

    npm

--- chapter_2/demo.js
+++ chapter_2/demo.js
@@ -1,4 +1,6 @@
 'use strict';
+const _ = require('underscore');
+
 const sentences = [
     { subject: 'JavaScript' , verb: 'is', object: 'great' },
     { subject: 'Elephants', verb: 'are', object: 'large'},
@@ -8,6 +10,7 @@ function say({ subject, verb, object}) {
     console.log(`${subject} ${verb} ${object}`);
 }
 
-for (let s of sentences) {
-    say(s);
+const verbs = _.pluck(sentences, 'verb');
+for (let verb of verbs) {
+    console.log(`found verb: ${verb}`);
 }
